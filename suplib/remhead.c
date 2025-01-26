3290144150e8d714d6e6974d933fcff9db935a65
Richard Downer <richard@downer.tech>
2025-01-26 21:24:11 +0000

    chore(suplib): file headers with license and copyright

--- suplib/remhead.c
+++ suplib/remhead.c
@@ -1,4 +1,14 @@
-
+/*
+ * DICE-NX project - https://dice-nx.dev/
+ *
+ * Copyright 1992-1997 Obvious Implementations Corp.
+ * Copyright 2023- DICE-NX project contributors
+ *
+ * Licensed under the BSD-3-Clause License, as written in the LICENSE file
+ * at the root of this project.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
 #include <stdio.h>
 #include "lists.h"
--- suplib/stricmp.c
+++ suplib/stricmp.c
@@ -1,8 +1,18 @@
+/*
+ * DICE-NX project - https://dice-nx.dev/
+ *
+ * Copyright 1990 Matthew Dillon
+ * Copyright 2023- DICE-NX project contributors
+ *
+ * Licensed under the BSD-3-Clause License, as written in the LICENSE file
+ * at the root of this project.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 
 /*
  *  STRICMP.C
- *
- *  (C)Copyright 1990, Matthew Dillon, All Rights Reserved
  */
 
 #include <string.h>
