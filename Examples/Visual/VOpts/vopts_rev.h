9a2531d0c6df7068ae67d903c691a166098b8066
Richard Downer <richard@downer.tech>
2026-01-25 17:05:46 +0000

    chore(examples): remove Visual as it is a duplicate of content in src

--- Examples/Visual/VOpts/vopts_rev.h
+++
@@ -1,6 +0,0 @@
-#define VERSION          3
-#define REVISION         2
-#define DATE    "15.3.93"
-#define VERS    "vopts 3.2"
-#define VSTRING "vopts 3.2 (15.3.93)"
-#define VERSTAG "\0$VER: vopts 3.2 (15.3.93)"
